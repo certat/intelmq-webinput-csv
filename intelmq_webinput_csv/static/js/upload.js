91e86330c4c62783a5ebcebb58eff2510ba018fd
Arno Esterhammer <esterhammer@cert.at>
2017-08-30 09:52:00 +0200

    fixed upload label

--- intelmq_webinput_csv/static/js/upload.js
+++ intelmq_webinput_csv/static/js/upload.js
@@ -1,3 +1,15 @@
-function test(){
-    console.log('This is the upload js');
-}
+var vm_upload = new Vue({
+    el: '#uploadApp',
+    fileInput: '#fileInput',
+    data: {
+        fileName: 'no file chosen',
+    },
+    methods:{
+        onChangeListener : function () {
+            if (fileInput.files.length === 1) {
+                this.fileName = fileInput.files[0].name;
+            }
+        },
+    },
+});
+
