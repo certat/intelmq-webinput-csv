faaf4202a4d72750d417fd3932091c3303bd86f2
Sebastian Wagner <wagner@cert.at>
2017-08-30 17:18:41 +0200

    ENH: Debug info for file submission

--- intelmq_webinput_csv/bin/backend.py
+++ intelmq_webinput_csv/bin/backend.py
@@ -57,6 +57,8 @@ def form():
 def upload_file():
     success = False
     if request.method == 'POST':
+        print('files:', list(request.files.keys()))
+        print('form data:', list(request.form.keys()))
         if 'file' in request.files and request.files['file'].filename:
             filedescriptor, filename = tempfile.mkstemp(suffix=".csv", text=True)
             request.files['file'].save(filename)
@@ -79,7 +81,12 @@ def upload_file():
         response.headers['Content-Type'] = "text/json; charset=utf-8"
         response.headers['Access-Control-Allow-Origin'] = "*"
         return response
-    return ''
+    else:
+        response = make_response('no file or text')
+        response.mimetype = 'application/json'
+        response.headers['Content-Type'] = "text/json; charset=utf-8"
+        response.headers['Access-Control-Allow-Origin'] = "*"
+        return response
 
 
 @app.route('/preview', methods=['GET', 'POST'])
