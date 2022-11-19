c47c7c4c53064c8e99bf350dd7d12b82cda84494
RoyK <rkokk@protonmail.com>
2022-11-19 22:17:40 +0100

    test: initial CSV model test

--- tests/test_models/test_csv.py
+++ tests/test_models/test_csv.py
@@ -30,8 +30,59 @@ line3.1,line3.2,line3.3
             'has_header': True
         }
 
-    def test_CSV(self, file1, parameters):
+    def test_CSV_creation(self, file1, parameters):
         with CSV.create(file1, **parameters) as csv:
-
             assert csv.num_lines == 3
             assert all(column in csv for column in ['column1', 'column2', 'column3'])
+
+            counter = 0
+            for index, line in csv:
+                counter += 1
+                assert index+1 == counter  # Validate index
+                assert len(line) == 3  # Validate all cells are returned
+                assert all(f"{counter}." in cell for cell in line)  # Validate correct cells are returned
+
+            # Ensure all three lines are read
+            assert counter == 3
+
+    def test_CSV_no_header(self, file1, parameters):
+        parameters['has_header'] = False
+        columns = ['column1', 'column2', 'column3']
+
+        with CSV.create(file1, **parameters) as csv:
+
+            assert csv.num_lines == 4
+            assert all(column not in csv for column in columns)
+
+            for index, (line_index, line) in enumerate(csv):
+                if line_index == 0:
+                    assert line == columns
+
+            assert index == 3
+
+    def test_CSV_max_lines(self, file1, parameters):
+        parameters['loadLinesMax'] = 2
+
+        with CSV.create(file1, **parameters) as csv:
+            assert csv.num_lines == 3
+
+            for index, (line_index, _) in enumerate(csv):
+                pass
+
+            # Ensure that only 2 records are returned
+            assert index == 1
+            assert line_index == 1
+
+    def test_CSV_skip_initial_space(self, file1, parameters):
+        # Test config with has_header and without
+        for has_header, skipInitialSpace in [(False, 2), (True, 1)]:
+            parameters['has_header'] = has_header
+            parameters['skipInitialSpace'] = skipInitialSpace
+
+            with CSV.create(file1, **parameters) as csv:
+                counter = 2
+                for line_index, line in csv:
+                    assert all(f"{counter}." in cell for cell in line)  # Validate correct cells are returned
+                    counter += 1
+
+                assert line_index == 1
